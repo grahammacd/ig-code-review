771cc0089860d71b9c319aa78b751360a86b7711
Graham MacDonald <graham.macdonald@incentivegmes.com>
2023-01-12 11:17:46 +0000

    Initial commit

---
+++ code-review/codeReview.js
@@ -0,0 +1,17 @@
+// Write a reusable function to return the score for a specific player
+// You have access to a scoreApiService and a method getScores which returns
+// an list of all scores.
+
+// Then call the function to return player with id 10 and write it to the console
+
+const scoreApiService = require("./scoreApiService");
+
+// Your answer (please review)
+function getPlayerScore()
+{
+    let score = scoreApiService.getScores().find(score => score.playerId == '10');
+    console.log(score);
+}
+
+getPlayerScore();
+// End: Your answer
---
+++ code-review/scoreApiService.js
@@ -0,0 +1,26 @@
+module.exports = {
+    getScores: () => {
+        return [
+            {
+                playerId: 1,
+                name: "Fred",
+                score: 20
+            },
+            {
+                playerId: 5,
+                name: "Susan",
+                score: 30
+            },
+            {
+                playerId: 8,
+                name: "Bob",
+                score: 40
+            },
+            {
+                playerId: 10,
+                name: "Jill",
+                score: 50
+            }
+       ]
+    }
+}
